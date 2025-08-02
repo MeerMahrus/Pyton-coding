de9991c77ba92074392c0eb586e1dcf676728fa3
MeerMahrus <devybluester@gmail.com>
2025-08-02 13:31:54 +0600

    Big calculator

---
+++ Big calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your first number"))
+b = int(input("Enter your second number"))
+c = int(input("Enter your third number"))
+d = int(input("Enter your forth number"))
+e = int(input("Enter your fith number"))
+
+print("Adition:",a+b+c+d+e)
