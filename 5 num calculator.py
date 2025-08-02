17581ed961242372769790033aea7837fb1c3a99
MeerMahrus <devybluester@gmail.com>
2025-08-02 10:08:50 +0600

    5 num calculator

---
+++ 5 num calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your first number:"))
+b = int(input("Enter your second number:"))
+
+print("Addition:",a+b)
+print("Subtaction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Remainder:",a%b)
