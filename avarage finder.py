2ea64ec29cf2f5570596dddf29e6b7d7c6835d8e
MeerMahrus <devybluester@gmail.com>
2025-08-06 15:37:21 +0600

    average

---
+++ avarage finder.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+c = int(input("Enter your third number: "))
+d = int(input("Enter your fourth number: "))
+e= int(input("Enter your fith number: "))
+
+total =a+b+c+d+e
+
+average= total/5
+
+print("The result is:",average)
---
+++ datat type.py
@@ -0,0 +1,26 @@
+data = 4
+print(type(data))
+
+data = 1.34
+print(type(data))
+
+data = "Animal"
+print(type(data))
+
+data = True
+print(type(data))
+
+data = [1, 2, 3]
+print(type(data))
+
+data = (1, 2, 3)
+print(type(data))
+
+data = {"a" : 1, "b" : 2}
+print(type(data))
+
+data = {1, 2, 3}
+print(type(data))
+
+data = None
+print(type(data))
