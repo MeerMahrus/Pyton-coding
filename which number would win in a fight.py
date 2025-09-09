a25e9e7e1925668d204dadf987dee502a2263032
MeerMahrus <devybluester@gmail.com>
2025-09-09 17:11:51 +0600

    hf

--- which number would win in a fight.py
+++ which number would win in a fight.py
@@ -2,9 +2,9 @@ W = int(input("Enter a your number: "))
 I = int(input("Enter a your number: "))
 N = int(input("Enter a your number: "))
 
-if W > I and W > N:
+if W < I and W < N:
     print("The Winner is....",W)
-elif I > W and I > N:
+elif I < W and I < N:
     print("The Winner is....",I)
 else:
     print("The Winner is....",N)
